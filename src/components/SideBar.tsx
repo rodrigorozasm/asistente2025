d7fa023e04f8803060b7d275d38988d96ba06312
nicolas gonzalez <nicolas.gonzalez@govic.cl>
2024-09-24 00:21:40 -0300

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -69,16 +69,6 @@ const Sidebar: React.FC<SidebarProps> = ({className, isSidebarCollapsed, toggleS
                     <PlusIcon {...iconProps} />
                     <span className="truncate">{t('new-chat')}</span>
                   </button>
-                  <Tooltip title={t('open-settings')} side="right" sideOffset={10}>
-                    <button
-                      type="button"
-                      className="flex px-3 min-h-[44px] py-1 gap-3 transition-colors duration-200 dark:text-white
-                      cursor-pointer text-sm rounded-md border dark:border-white/20 hover:bg-gray-500/10 h-11 w-11
-                      flex-shrink-0 items-center justify-center bg-white dark:bg-transparent"
-                      onClick={openSettingsDialog}>
-                      <Cog8ToothIcon/>
-                    </button>
-                  </Tooltip>
                   <Tooltip title={t('close-sidebar')} side="right" sideOffset={10}>
                     <button
                       className="flex px-3 min-h-[44px] py-1 gap-3 transition-colors duration-200 dark:text-white
